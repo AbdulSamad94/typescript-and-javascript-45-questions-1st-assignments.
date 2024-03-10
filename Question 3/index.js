d0a8f3f434a6641ab0a39962f7566bf7e971f1dd
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 17:51:22 +0500

    Add files via upload

---
+++ Question 3/index.js
@@ -0,0 +1,4 @@
+// 3rd Question solved
+var a = "Abdul Samad Siddiqui";
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
---
+++ Question 3/index.ts
@@ -0,0 +1,8 @@
+
+// 3rd Question solved
+
+
+
+let a = "Abdul Samad Siddiqui"
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
