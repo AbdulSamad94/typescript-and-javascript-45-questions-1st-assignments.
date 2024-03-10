f714b8f191ca03e73c93cc0d3f977096acb86d7e
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 18:05:39 +0500

    Delete Question 12 directory

--- Question 12/index.js
+++
@@ -1,7 +0,0 @@
-//12th question solved
-var friendsName = ["Samad ", "Faiz ", "Hunain ", "Abdullah "];
-var msg = "Hi! how are you?";
-console.log(friendsName[0] + msg);
-console.log(friendsName[1] + msg);
-console.log(friendsName[2] + msg);
-console.log(friendsName[3] + msg);
