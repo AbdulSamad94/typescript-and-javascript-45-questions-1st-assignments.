9c22ad63e526da904fca28df581a7f6a630544ee
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 17:50:53 +0500

    Add files via upload

---
+++ Question 2/index.js
@@ -0,0 +1,3 @@
+// 2nt Question solved
+var person = "Hello my name is 'Abdul Samad'";
+console.log(person);
---
+++ Question 2/index.ts
@@ -0,0 +1,6 @@
+// 2nt Question solved
+
+
+
+let person = "Hello my name is 'Abdul Samad'"
+console.log(person)
