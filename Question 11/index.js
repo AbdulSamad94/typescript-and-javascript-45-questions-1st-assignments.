2f0f6d0017828eb31bdf38bdd183d9ce0f5cad22
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 18:05:06 +0500

    Delete Question 11 directory

--- Question 11/index.js
+++
@@ -1,6 +0,0 @@
-// Question 11 solved
-var friendName = ["Samad", "Faiz", "Hunain", "Abdullah"];
-console.log(friendName[0]);
-console.log(friendName[1]);
-console.log(friendName[2]);
-console.log(friendName[3]);
