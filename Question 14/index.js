ba61e578c0ad80973d459467dcfe6355f2720fad
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 18:06:07 +0500

    Delete Question 14 directory

--- Question 14/index.js
+++
@@ -1,3 +0,0 @@
-// 14th question solved
-var guests = ["Faiz", "Anas", "Hunain"];
-guests.map(function (items) { return console.log("Hi! ".concat(items, " , would you like to have dinner together?")); });
--- Question 14/index.ts
+++
@@ -1,7 +0,0 @@
-// 14th question solved
-
-let guests = ["Faiz", "Anas" , "Hunain"]
-guests.map((items)=> console.log(`Hi! ${items} , would you like to have dinner together?`))
-
-
-
