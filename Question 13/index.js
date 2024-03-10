ab22f131e75420060583b8e86c0a1a859f6c2dec
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 18:05:55 +0500

    Delete Question 13 directory

--- Question 13/index.js
+++
@@ -1,3 +0,0 @@
-// 13th question solved 
-var transportation = ["Audi", "Honda Motorcycle", "GTR"];
-transportation.map(function (items) { return console.log("I would like to own a ".concat(items)); });
--- Question 13/index.ts
+++
@@ -1,7 +0,0 @@
-// 13th question solved 
-
-
-
-let transportation = ["Audi", "Honda Motorcycle", "GTR"]
-transportation.map((items)=> console.log(`I would like to own a ${items}`))
-
