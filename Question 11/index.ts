d15c34b0b42e57b698062df1104e8c6be4b75efc
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 18:07:59 +0500

    Add files via upload

---
+++ Question 11/index.ts
@@ -0,0 +1,9 @@
+// Question 11 solved
+
+
+
+let friendName= ["Samad","Faiz","Hunain","Abdullah"];
+console.log(friendName[0]);
+console.log(friendName[1]);
+console.log(friendName[2]);
+console.log(friendName[3]);
