c0e8f3e7283dac11683e4263727e29280cacb3b2
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-10 18:04:10 +0500

    Delete Question 10 directory

--- Question 10/index.js
+++
@@ -1,4 +0,0 @@
-//10th question solved
-var name_1 = "Abdul Samad";
-// i gave it a name, now i will print it
-console.log(name_1);
--- Question 10/index.ts
+++
@@ -1,7 +0,0 @@
-//10th question solved
-
-
-
-let name_1 = "Abdul Samad"
-// i gave it a name, now i will print it
-console.log(name_1)
